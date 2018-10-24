669509946e246df69cef2fd96cc9da7d77b93a1d
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-10-24 14:44:19 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
+  subscription_id = "${var.azure_subscription_id}"
+  tenant_id       = "${var.azure_tenant_id}"
+}
+
+resource "azurerm_resource_group" "network" {
+  name     = "production"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = "production-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.network.location}"
+  resource_group_name = "${azurerm_resource_group.network.name}"
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
+}
