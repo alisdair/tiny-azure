3177c70b6e5b2cd281fa7ea0d7bcc8d409b43930
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-10-24 14:50:36 -0400

    Add named subnet and NIC

--- network.tf
+++ network.tf
@@ -16,3 +16,22 @@ resource "azurerm_virtual_network" "network" {
   location            = "${azurerm_resource_group.network.location}"
   resource_group_name = "${azurerm_resource_group.network.name}"
 }
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.network.name}"
+  virtual_network_name = "${azurerm_virtual_network.network.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "internal-nic"
+  location            = "${azurerm_resource_group.network.location}"
+  resource_group_name = "${azurerm_resource_group.network.name}"
+
+  ip_configuration {
+    name                          = "config"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "dynamic"
+  }
+}
