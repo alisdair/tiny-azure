5b5816feec7d738f23a319ad51b2b13d11176ed7
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-10-24 14:48:51 -0400

    Remove subnets

--- network.tf
+++ network.tf
@@ -15,19 +15,4 @@ resource "azurerm_virtual_network" "network" {
   address_space       = ["10.0.0.0/16"]
   location            = "${azurerm_resource_group.network.location}"
   resource_group_name = "${azurerm_resource_group.network.name}"
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
-
-  subnet {
-    name           = "subnet3"
-    address_prefix = "10.0.3.0/24"
-  }
 }
